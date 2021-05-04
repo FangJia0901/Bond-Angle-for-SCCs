5b4cb5ce26073514be289f8f5c445a8e5a8d3629
trailblazer1024 <50763213+FangJia0901@users.noreply.github.com>
2021-05-04 20:13:25 +0800

    layernorm
    Class overriding pytorch default layernorm intitialization

---
+++ layernorm.py
@@ -0,0 +1,11 @@
+import torch.nn as nn
+
+# The below layernorm class initializes parameters according to the default 
+# initialization of bacthnorm layers in pytorch v1.1 and below. Somehow this 
+# initialization seemed to work beter.
+class LayerNorm(nn.LayerNorm):
+    """Class overriding pytorch default layernorm intitialization."""
+    def reset_parameters(self):
+        if self.elementwise_affine:
+            nn.init.uniform_(self.weight)
+            nn.init.zeros_(self.bias)
