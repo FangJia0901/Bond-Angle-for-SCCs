e1abfb8a66623d3357e8dca8e4e53eb1aada34ec
trailblazer1024 <50763213+FangJia0901@users.noreply.github.com>
2021-05-11 16:37:44 +0800

    displot of SCCs
    displot of SCCs

--- distplot.py
+++ distplot.py
@@ -2,10 +2,9 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import constants as C
-#sns.set_style("whitegrid")
 
 all_df = pd.read_csv(C.PROC_DATA_PATH + 'all_df.csv', index_col=0)
 plt.figure(figsize=(10,3))                                                               
 sns.distplot(all_df['scalar_coupling_constant'])
 plt.title('Displot of scalar coupling constant')
-plt.savefig('./Distplot.png', dpi=600, bbox_inches='tight')
+plt.savefig('./Distplot.png', dpi=900, bbox_inches='tight')
